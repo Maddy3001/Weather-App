3a673288c956909f906df47a26491fbc514f2bb7
maddy3001 <mjha6741@gmail.com>
2022-02-04 02:00:29 +0530

    initial

---
+++ script.js
@@ -0,0 +1,29 @@
+var city = document.querySelector("#cities");
+var button = document.querySelector("#buttons");
+var cityname = document.querySelector("#cout")
+var desc = document.querySelector("#para")
+var temperature = document.querySelector("#temp")
+var wind = document.querySelector("#wind")
+var api = "c24e73f26496e4a539882534b2f699db"
+
+function convert(e){
+    return (e-273.15).toFixed(2)
+}
+button.addEventListener("click",()=>{
+    var city2 = city.value
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city2}&appid=${api}`)
+    .then(res=>res.json())
+    .then(data =>{
+        var citi = data['name']
+        var disc = data['weather']['0']['description']
+        var tamp = data['main']['temp']
+        var winds = data['wind']['speed']
+        cityname.innerHTML= `current city: ${citi}`
+        temperature.innerHTML=`current temperature: ${convert(tamp)}`
+        desc.innerHTML=`mood: ${disc}`
+        wind.innerHTML=`current wind: ${winds}`
+    })
+    .catch(()=>{
+        alert("chutiya galat daala hai")
+    })
+})
